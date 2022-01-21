89daaa59d8ab4f42263947f9ee12dc249e4e4f37
Roland Schenk <roland.schenk@gmx.net>
2022-01-21 14:41:23 +0100

    changes for hoermoos

--- Sss.Umb9.Mutobo/Services/NavigationService.cs
+++ Sss.Umb9.Mutobo/Services/NavigationService.cs
@@ -61,33 +61,23 @@ namespace Sss.Umb9.Mutobo.Services
                 Url = parentNode.Value<bool>(DocumentTypes.BasePage.Fields.NotClickable) ? "#" : parentNode.GetDitUrl(),
                 NewWindow = parentNode.GetOpenInNewWindowFlag(),
                 IsSearchPage = parentNode.ContentType.Alias == DocumentTypes.SearchResults.Alias
-
-
-
             };
 
             var childNavItems = new List<NavItem>();
-
-
 
             foreach (var childNode in parentNode.Children.Where(c => !(new BasePage(c)).HideFromNavigation))
             {
                 if (childNode.IsComposedOf(DocumentTypes.BasePage.Alias))
                 {
-
-
                     childNavItems.Add(new NavItem()
                     {
                         Title = childNode.Name,
                         Url = childNode.Value<bool>(DocumentTypes.BasePage.Fields.NotClickable) ? "#" : childNode.GetDitUrl(),
                         NewWindow = childNode.GetOpenInNewWindowFlag(),
                         IsSearchPage = childNode.ContentType.Alias == DocumentTypes.SearchResults.Alias
-
                     });
-
                 }
             }
-
 
             result.Children = childNavItems;
             return result;
